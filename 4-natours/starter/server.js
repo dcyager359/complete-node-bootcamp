29279c2cda769eaafc7da92cfebda2d4f178db9a
Chris Yager <chris.yager@octo.us>
2022-11-09 09:22:00 -0500

    hook up mongo db

--- 4-natours/starter/server.js
+++ 4-natours/starter/server.js
@@ -1,4 +1,25 @@
+const mongoose = require('mongoose');
 require('dotenv').config({ path: './config.env' });
+
+const dbCloud = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+// const dbLocal = process.env.DATABASE_LOCAL.replace(
+//   '<PASSWORD>',
+//   process.env.DATABASE_PASSWORD
+// );
+
+mongoose
+  .connect(dbCloud, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => {
+    console.log('Connected to MongoDB');
+  });
+
 const app = require('./app');
 
 const port = process.env.PORT || 8000;
