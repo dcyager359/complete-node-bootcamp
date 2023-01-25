b42cba3be9aab2f09991842b326206beeb6b777f
Chris Yager <chris.yager@octo.us>
2023-01-25 10:23:09 -0500

    add error handling for invalid routes

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -17,12 +17,29 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use((req, res, next) => {
-  // req.msg = 'Hello from the middleware 👋';
-  next();
-});
-
+//handle valid routes
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+
+//handle invalid request
+app.all('*', (req, res, next) => {
+  // res.status(404).json({
+  //   status: 'fail',
+  //   message: `Can't find route ${req.originalUrl} on this server`,
+  // });
+  const err = new Error(`Can't find route ${req.originalUrl} on this server`);
+  err.status = 'fail';
+  err.statusCode = 404;
+  next(err);
+});
+
+app.use((err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'error';
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
+  });
+});
 
 module.exports = app;
